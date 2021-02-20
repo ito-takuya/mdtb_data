6667f2a961bda92824bff6c3a650ef1aa5307195
Taku Ito <taku.ito1@gmail.com>
2021-02-20 14:55:30 -0500

    update

--- scripts/preproc_qunex_turnkey_v2.sh
+++ scripts/preproc_qunex_turnkey_v2.sh
@@ -28,17 +28,15 @@ session_suffixes="a1 a2 b1 b2"
 sessions="02_a1,02_a2,02_b1,02_b2,03_a1,03_a2,03_b1,03_b2,04_a1,04_a2,04_b1,04_b2,06_a1,06_a2,06_b1,06_b2,09_a1,09_a2,09_b1,09_b2,12_a1,12_a2,12_b1,12_b2,15_a1,15_a2,15_b1,15_b2,18_a1,18_a2,18_b1,18_b2,20_a1,20_a2,20_b1,20_b2,22_a1,22_a2,22_b1,22_b2,25_a1,25_a2,25_b1,25_b2,27_a1,27_a2,27_b1,27_b2,29_a1,29_a2,29_b1,29_b2,31_a1,31_a2,31_b1,31_b2,08_a1,08_a2,08_b1,08_b2,10_a1,10_a2,10_b1,10_b2,14_a1,14_a2,14_b1,14_b2,17_a1,17_a2,17_b1,17_b2,19_a1,19_a2,19_b1,19_b2,21_a1,21_a2,21_b1,21_b2,24_a1,24_a2,24_b1,24_b2,26_a1,26_a2,26_b1,26_b2,28_a1,28_a2,28_b1,28_b2,30_a1,30_a2,30_b1,30_b2"
 sessions_arr="02_a1 02_a2 02_b1 02_b2 03_a1 03_a2 03_b1 03_b2 04_a1 04_a2 04_b1 04_b2 06_a1 06_a2 06_b1 06_b2 09_a1 09_a2 09_b1 09_b2 12_a1 12_a2 12_b1 12_b2 15_a1 15_a2 15_b1 15_b2 18_a1 18_a2 18_b1 18_b2 20_a1 20_a2 20_b1 20_b2 22_a1 22_a2 22_b1 22_b2 25_a1 25_a2 25_b1 25_b2 27_a1 27_a2 27_b1 27_b2 29_a1 29_a2 29_b1 29_b2 31_a1 31_a2 31_b1 31_b2 08_a1 08_a2 08_b1 08_b2 10_a1 10_a2 10_b1 10_b2 14_a1 14_a2 14_b1 14_b2 17_a1 17_a2 17_b1 17_b2 19_a1 19_a2 19_b1 19_b2 21_a1 21_a2 21_b1 21_b2 24_a1 24_a2 24_b1 24_b2 26_a1 26_a2 26_b1 26_b2 28_a1 28_a2 28_b1 28_b2 30_a1 30_a2 30_b1 30_b2"
 
-#sessions="02_b2"
-#sessions_arr="02_b2"
-#sessions="02_b2_nofmap"
-#sessions_arr="02_b2_nofmap"
+#sessions="02_a1,02_a2,02_b1,02_b2,04_a1,04_a2,04_b1,04_b2,08_a1,09_a1,09_b2,10_b2,15_b2,18_a1,21_a1,24_a1,24_a2,24_b1,24_b2,25_a1,25_a2,25_b2,28_a1,28_b1,28_b2"
+#sessions_arr="02_a1 02_a2 02_b1 02_b2 04_a1 04_a2 04_b1 04_b2 08_a1 09_a1 09_b2 10_b2 15_b2 18_a1 21_a1 24_a1 24_a2 24_b1 24_b2 25_a1 25_a2 25_b2 28_a1 28_b1 28_b2"
 
 ## Session names, excluding 08_a1 and 17_a2
 #sessions="02_a1,02_a2,02_b1,02_b2,03_a1,03_a2,03_b1,03_b2,04_a1,04_a2,04_b1,04_b2,06_a1,06_a2,06_b1,06_b2,09_a1,09_a2,09_b1,09_b2,12_a1,12_a2,12_b1,12_b2,15_a1,15_a2,15_b1,15_b2,18_a1,18_a2,18_b1,18_b2,20_a1,20_a2,20_b1,20_b2,22_a1,22_a2,22_b1,22_b2,25_a1,25_a2,25_b1,25_b2,27_a1,27_a2,27_b1,27_b2,29_a1,29_a2,29_b1,29_b2,31_a1,31_a2,31_b1,31_b2,02_a1,02_a2,02_b1,02_b2,04_a1,04_a2,04_b1,04_b2,08_a2,08_b1,08_b2,10_a1,10_a2,10_b1,10_b2,14_a1,14_a2,14_b1,14_b2,17_a1,17_b1,17_b2,19_a1,19_a2,19_b1,19_b2,21_a1,21_a2,21_b1,21_b2,24_a1,24_a2,24_b1,24_b2,26_a1,26_a2,26_b1,26_b2,28_a1,28_a2,28_b1,28_b2,30_a1,30_a2,30_b1,30_b2"
 #sessions_arr="02_a1 02_a2 02_b1 02_b2 03_a1 03_a2 03_b1 03_b2 04_a1 04_a2 04_b1 04_b2 06_a1 06_a2 06_b1 06_b2 09_a1 09_a2 09_b1 09_b2 12_a1 12_a2 12_b1 12_b2 15_a1 15_a2 15_b1 15_b2 18_a1 18_a2 18_b1 18_b2 20_a1 20_a2 20_b1 20_b2 22_a1 22_a2 22_b1 22_b2 25_a1 25_a2 25_b1 25_b2 27_a1 27_a2 27_b1 27_b2 29_a1 29_a2 29_b1 29_b2 31_a1 31_a2 31_b1 31_b2 02_a1 02_a2 02_b1 02_b2 04_a1 04_a2 04_b1 04_b2 08_a2 08_b1 08_b2 10_a1 10_a2 10_b1 10_b2 14_a1 14_a2 14_b1 14_b2 17_a1 17_b1 17_b2 19_a1 19_a2 19_b1 19_b2 21_a1 21_a2 21_b1 21_b2 24_a1 24_a2 24_b1 24_b2 26_a1 26_a2 26_b1 26_b2 28_a1 28_a2 28_b1 28_b2 30_a1 30_a2 30_b1 30_b2"
 #
-#sessions="08_a1,17_a2"
-#sessions_arr="08_a1 17_a2"
+sessions="09_a1,09_a2,09_b1"
+sessions_arr="09_a1 09_a2 09_b1"
 
 # - Type of QuNex Turnkey run
 RUNTURNKEY_TYPE="local"
@@ -91,6 +89,7 @@ fi
 
 # - QuNex Turnkey steps -- HCP SET UP
 runturnkey_setup="createSessionInfo,createBatch,setupHCP"
+runturnkey_setup="createSessionInfo,setupHCP"
 #runturnkey_setup="setupHCP"
 execute=0
 if [ $execute -eq 1 ]; then
@@ -165,7 +164,8 @@ fi
 # MOST IMPORTANT STEPS - RUN PREPROCESSING VIA TURNKEY on scheduler
 # - QuNex Turnkey steps
 runturnkey_hcp="hcp1,hcp2,hcp3,hcp4,hcp5"
-execute=1
+#runturnkey_hcp="hcp4,hcp5"
+execute=0
 if [ $execute -eq 1 ]; then
 
 for sess in $sessions_arr
@@ -184,7 +184,7 @@ do
         --turnkeysteps="$runturnkey_hcp" \
         --overwritestep="no" \
         --container="$qunex_container" \
-        --scheduler="SLURM,time=3-00:00:00,ntasks=1,cpus-per-task=1,mem-per-cpu=8000,partition=pi_anticevic,partition=week,account=anticevic" 
+        --scheduler="SLURM,time=5-00:00:00,ntasks=1,cpus-per-task=1,mem-per-cpu=8000,partition=pi_anticevic,partition=week,account=anticevic" 
 
 done
 fi
@@ -211,7 +211,7 @@ do
         --mappingfile="$my_study_folder/sessions/specs/mdtb2hcp_mapping_2fmapmag.txt" \
         --projectname="$my_study" \
         --turnkeysteps="$runturnkey_hcp" \
-        --overwritestep="no" \
+        --overwritestep="yes" \
         --container="$qunex_container" \
         --scheduler="SLURM,time=1-00:00:00,ntasks=1,cpus-per-task=1,mem-per-cpu=8000,partition=pi_anticevic,partition=day,account=anticevic" 
 
@@ -234,7 +234,7 @@ if [ $execute -eq 1 ]; then
 
 fi
 
-execute=0
+execute=1
 if [ $execute -eq 1 ]; then
 
     for session in $sessions_arr
